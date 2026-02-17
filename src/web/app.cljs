9a940b57b2e95ff155d3f9f5260bf1f1e6b4729b
Braeden Morrison <b.j.morrison20@gmail.com>
2026-02-16 20:10:12 -0500

    Add more output to the webpage

--- src/web/app.cljs
+++ src/web/app.cljs
@@ -28,15 +28,19 @@
 
     ; Clear output
     (set-text! "error" "")
-    (set-value! "output" "")
+    (set-text! "match-error" "")
+    (set-value! "parsed-output" "")
+    (set-value! "compiled-output" "")
+    (set-value! "assembled-output" "")
 
     (try
         (let [regex    (value-of "regex-input")
               ast      (parser/parse-regex regex)
               prog     (codegen/code-gen ast)
-              asm      (assembler/assemble prog)
-              asm-text (codegen/assembly asm)]
-            (set-value! "output" asm-text))
+              asm      (assembler/assemble prog)]
+            (set-value! "parsed-output" (js/JSON.stringify (clj->js ast) nil 2))
+            (set-value! "compiled-output" (codegen/assembly prog))
+            (set-value! "assembled-output" (codegen/assembly asm)))
 
         (catch js/Error e
             ;; ex-info shows up as js/Error in CLJS
@@ -50,3 +54,8 @@
     (by-id "compile-btn")
     "click"
     compile-regex!)
+
+(.addEventListener
+    (by-id "match-btn")
+    "click"
+    #(set-text! "match-error" "Not Implemented!"))
