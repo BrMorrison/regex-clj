96cb4751e3415a023a2849e71f87fde323c281d6
Braeden Morrison <b.j.morrison20@gmail.com>
2026-02-21 10:12:03 -0500

    Add the ability to step through evaluation

--- src/regex_vm/vm.cljc
+++ src/regex_vm/vm.cljc
@@ -1,32 +1,47 @@
 (ns regex-vm.vm
     "A virtual machine for executing regular expressions")
 
-(defrecord VMThread [pc])
+(defrecord VMState [threads sp matched?])
 
-(defn- advance [thread] (->VMThread (inc (:pc thread))))
-
-(defn- inst-at [prog thread] (nth prog (:pc thread)))
+(defn- inst-at [prog thread] (nth prog thread))
 
 (defn- char-matches? [ch prog thread]
     (let [inst (inst-at prog thread)]
         (and (= :char (:op inst))
              (= (str ch) (:char inst)))))
 
-(defn- is-match? [prog thread] (= :match (:op (inst-at prog thread))))
+(defn- ec-needed? [prog threads]
+    "Check if the VM needs to execute an epsilon closure"
+    (some #(let [op (:op (inst-at prog %))]
+                (or (= :split op)
+                    (= :jmp   op)))
+            threads))
+
+(defn- is-match? [prog threads]
+    "Check if the VM is currently in a matched state"
+    (boolean
+        (some #(= :match (:op (inst-at prog %)))
+              threads)))
 
 (defn- epsilon-closure
     "Calculates the epsilon closure of the program for the initial set of threads.
      This determines which program states are reachable without consuming input."
     [prog init-threads]
-    (loop [worklist (set (map #(:pc %) init-threads))
+
+    ; Work with sets to avoid duplicate threads
+    (loop [worklist (set init-threads)
            visited #{}
            result #{}]
         (if (empty? worklist)
-            (set (map #(->VMThread %) result))
+            result
+
+            ; Pop the next thread off the queue and get the corresponding instruction
             (let [pc (first worklist)
                 worklist (disj worklist pc)
                 inst (nth prog pc)]
-                (if (contains? visited pc)
+
+                ; Don't process it if it's a program state we've already seen
+                (if (contains? visited pc) ;
                     (recur worklist visited result)
                     (let [visited (conj visited pc)]
                         (case (:op inst)
@@ -43,25 +58,34 @@
                             (:char :match)
                             (recur worklist visited (conj result pc)))))))))
 
-(defn- step-char [prog initial-threads ch]
-    "Executes the regular expression matching for a single character of input
-     based on the initial program threads."
-    (let [threads (epsilon-closure prog initial-threads)
-          match-found (some #(= :match (:op (inst-at prog %))) threads)]
-        (if match-found
-            threads
-            (epsilon-closure prog
-                             (keep #(when (char-matches? ch prog %) (advance %)) threads)))))
+(def init-state (->VMState #{0} 0 false))
 
-(defn match? [s prog]
-    "Checks if the provided string is matched by the compiled regex program."
-    (let [ prog (vec prog)
-           initial-threads (epsilon-closure prog #{(->VMThread 0)})]
-        (loop [threads initial-threads
-               chars (seq s)]
-            (cond
-                (some (partial is-match? prog) threads) true
-                (empty? threads) false
-                (empty? s) false
-                :else (recur (step-char prog threads (first chars))
-                             (rest chars))))))
+(defn step
+    "Performs a single execution state on the VM and returns an updated state."
+    [prog s state]
+
+    (if (ec-needed? prog (:threads state))
+
+        ; If the epsilon closure does something, then do that as the step. 
+        (let [ec-threads (epsilon-closure prog (:threads state))]
+            (->VMState ec-threads (:sp state) (is-match? prog ec-threads)))
+
+        ; Otherwise, consume a token of input and make that the step.
+        (let [sp (:sp state)
+              ch (nth s sp)
+              threads (keep #(when (char-matches? ch prog %) (inc %)) (:threads state))]
+            (->VMState threads (inc sp) (is-match? prog threads)))))
+
+(defn done?
+    [prog s state]
+    (let [threads (:threads state)]
+        (or (empty? threads)
+            (and (not (ec-needed? prog threads))
+                 (= (count s) (:sp state))))))
+
+(defn run
+    [prog s state]
+    (loop [state state]
+        (if (done? prog s state)
+            state
+            (recur (step prog s state)))))
--- src/web/app.cljs
+++ src/web/app.cljs
@@ -13,75 +13,177 @@
 (defn by-id [id]
     (.getElementById js/document id))
 
-(defn value-of [id]
-    (.-value (by-id id)))
+(defn set-value! [obj v]
+    (set! (.-value obj) v))
 
-(defn set-value! [id v]
-    (set! (.-value (by-id id)) v))
+(defn set-text! [obj v]
+    (set! (.-textContent obj) v))
 
-(defn set-text! [id v]
-    (set! (.-textContent (by-id id)) v))
+(defn enable! [obj]
+    (set! (.-disabled obj) false))
+
+(defn disable! [obj]
+    (set! (.-disabled obj) true))
 
 ;; ---------------------------------------------------------------------
-;; Compilation Pipeline
+;; Page Elements
 ;; ---------------------------------------------------------------------
 
-(defn clear-match-output! []
-    (set-text! "match-error" "")
-    (set-text! "match-success" ""))
+; -- Text Inputs --
+(def regex-input    (by-id "regex-input"))
+(def string-input   (by-id "string-input"))
+(def assembly-input (by-id "assembly-input"))
+
+; -- Text Outputs --
+(def parsed-output    (by-id "parsed-output"))
+(def compiled-output  (by-id "compiled-output"))
+(def assembled-output (by-id "assembled-output"))
+(def evaluator-output (by-id "evaluator-output"))
+
+; -- Success/Error Messages --
+(def compile-error (by-id "compile-error"))
+(def match-error   (by-id "match-error"))
+(def match-success (by-id "match-success"))
+
+; -- Buttons --
+(def compile-btn (by-id "compile-btn"))
+(def match-btn   (by-id "match-btn"))
+(def start-btn   (by-id "start-btn"))
+(def reset-btn   (by-id "reset-btn"))
+(def step-btn    (by-id "step-btn"))
+(def run-btn     (by-id "run-btn"))
+
+;; ---------------------------------------------------------------------
+;; Page Helpers
+;; ---------------------------------------------------------------------
 
 (defn clear-compile-output! []
-    (clear-match-output!)
-    (set-text! "error" "")
-    (set-value! "parsed-output" "")
-    (set-value! "compiled-output" "")
-    (set-value! "assembled-output" ""))
+    (set-text! compile-error "")
+    (set-value! parsed-output "")
+    (set-value! compiled-output "")
+    (set-value! assembled-output ""))
+
+(defn run-safe [f out]
+    (try 
+        (f)
+        (catch js/Error e
+            (js/console.error e)
+            (set-text! out (.-message e)))))
+
+
+;; ---------------------------------------------------------------------
+;; Compilation
+;; ---------------------------------------------------------------------
 
 (defn compile-regex! []
 
     (clear-compile-output!)
 
-    (try
-        (let [regex    (value-of "regex-input")
-              ast      (parser/parse-regex regex)
-              prog     (codegen/code-gen ast)
-              asm      (assembler/assemble prog)]
-            (set-value! "parsed-output" (js/JSON.stringify (clj->js ast) nil 2))
-            (set-value! "compiled-output" (inst/assembly prog))
-            (set-value! "assembled-output" (inst/assembly asm)))
+    (let [regex  (.-value regex-input)
+            ast  (parser/parse-regex regex)
+            prog (codegen/code-gen ast)
+            asm  (assembler/assemble prog)]
+        (set-value! parsed-output (js/JSON.stringify (clj->js ast) nil 2))
+        (set-value! compiled-output (inst/assembly prog))
+        (set-value! assembled-output (inst/assembly asm))
+        (set-value! assembly-input (inst/assembly asm))))
 
-        (catch js/Error e
-            (set-text! "error" (.-message e)))))
+;; ---------------------------------------------------------------------
+;; Evaluation
+;; ---------------------------------------------------------------------
 
-(defn evaluate-regex! []
+(defn clear-eval-output! []
+    (set-text! match-error "")
+    (set-text! match-success "")
+    (set-value! evaluator-output ""))
 
-    (clear-match-output!)
+(defn read-evaluator-inputs []
+    [(.-value string-input)
+     (inst/parse-assembled (.-value assembly-input))])
 
-    (try
-        (let [s        (value-of "string-input")
-              program  (inst/parse-assembled (value-of "assembled-output"))]
+(defn set-eval-state! [state]
+    (def eval-state state))
 
-            ; Update the div based on the result of the match
-            (if (vm/match? s program)
-                (set-text! "match-success" "Matched!")
-                (set-text! "match-error" "No Match!")))
+(defn set-eval-output! [state]
+    ; Update the div based on the result of the match
+    (if (:matched? state)
+        (set-text! match-success "Matched!")
+        (set-text! match-error "No Match!")))
 
-    (catch :default e
-        (set-text!
-            "match-error"
-            (str "Error: " (.-message e))))))
+(defn start-eval! []
+    (disable! start-btn)
+    (enable!  reset-btn)
+    (enable!  step-btn)
+    (enable!  run-btn)
+    (set! (.-readonly string-input) true)
+    (set! (.-readonly assembly-input) true)
+    (clear-eval-output!)
+    (set-eval-state! vm/init-state))
 
+(defn stop-eval! []
+    (disable! reset-btn)
+    (disable! step-btn)
+    (disable! run-btn)
+    (enable!  start-btn)
+    (set! (.-readonly string-input) false)
+    (set! (.-readonly assembly-input) false))
+
+(defn check-match! []
+    (clear-eval-output!)
+
+    (let [[s program] (read-evaluator-inputs)
+            final-state (vm/run program s vm/init-state)]
+        (set-eval-output! final-state)))
+
+(defn run-evaluator! []
+    (let [[s program] (read-evaluator-inputs)
+            final-state (vm/run program s eval-state)]
+
+        (set-eval-output! final-state)
+        (stop-eval!)))
+
+(defn step-evaluator! []
+
+    (clear-eval-output!)
+
+    (let [[s program] (read-evaluator-inputs)
+          next-state (vm/step program s eval-state)]
+
+        (if (vm/done? program s next-state)
+            (do (set-eval-output! next-state)
+                (stop-eval!))
+            (set-eval-state! next-state))))
 
 ;; ---------------------------------------------------------------------
 ;; Entry Point
 ;; ---------------------------------------------------------------------
 
 (.addEventListener
-    (by-id "compile-btn")
+    compile-btn
     "click"
-    compile-regex!)
+    #(run-safe compile-regex! compile-error))
 
 (.addEventListener
-    (by-id "match-btn")
+    match-btn
     "click"
-    evaluate-regex!)
+    #(run-safe check-match! match-error))
+
+(.addEventListener
+    start-btn
+    "click"
+    start-eval!)
+
+(.addEventListener
+    reset-btn
+    "click"
+    #(set-eval-state! vm/init-state))
+
+(.addEventListener
+    step-btn
+    "click"
+    #(run-safe step-evaluator! match-error))
+
+(.addEventListener
+    run-btn
+    "click"
+    #(run-safe run-evaluator! match-error))
